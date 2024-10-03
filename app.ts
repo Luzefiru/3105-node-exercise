dc7767253a8f3f5281c75d070cdded29623e30a9
Luzefiru <ian.deejay.iv@gmail.com>
2024-10-03 19:34:40 +0800

    feat: add tsconfig & dev scripts

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ app.get('/', (_, res) => {
   return res.json({ hello: 'world' });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port || 3000, () => {
+  console.log(`🚀 Server started on port: ${port}`);
 });
